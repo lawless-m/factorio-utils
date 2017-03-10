91f8d3aacb3bbbed328d1d9dd9086a22569de6e7
lawless-m <maht0x0r@xsmail.com>
2017-03-10 13:51:19 -0500

    refectorred

--- oil.jl
+++ oil.jl
@@ -2,21 +2,84 @@ using JuMP
 
 m = Model()
 
-@variable(m, oil >= 1, Int)
-@variable(m, h2l >= 1, Int)
-@variable(m, l2g >= 1, Int)
+type Recipe
+	name::AbstractString
+	#nfacs::Int64
+	time::Float64
+	outs::Dict{AbstractString, Float64}
+	ins::Dict{AbstractString, Float64}
+	#Recipe(n, t) = new(n, 0, t, Dict{AbstractString, Float64}(), Dict{AbstractString, Float64}())
+	Recipe(name, time, speed) = new(name, time/speed, Dict{AbstractString, Float64}(), Dict{AbstractString, Float64}())
+end
 
-@constraint(m, 10oil <= 240)
-@constraint(m, 5.5oil + 2l2g <= 240)
-@constraint(m, 4.5oil +  3h2l <= 3l2g)
-@constraint(m, 4oil <= h2l)
+function production(r::Recipe, nfacs)
+	@printf "%s: %d\n" r.name nfacs
+	@printf "\tProduces\n"
+	for (k,v) in r.outs
+		@printf "\t\t%0.2f %s per min\n" 60v * nfacs k
+	end
+	@printf "\tConsumes\n"
+	for (k,v) in r.ins
+		@printf "\t\t%0.2f %s per min\n" 60v * nfacs k
+	end
+end
 
-@objective(m, Min, oil + h2l + l2g)
+@enum Rnum AOP HOC LOC SOL ROK
+
+recips = Dict{Rnum, Recipe}()
+
+macro IN(rnum, iname, iqty)
+	:(recips[$rnum].ins[$iname] = $iqty / recips[$rnum].time)
+end
+
+macro OUT(rnum, iname, iqty)
+	:(recips[$rnum].outs[$iname] = $iqty / recips[$rnum].time)
+end
+
+recips[AOP] = Recipe("Advanced Oil Processing", 5.0, 1 * 1.5 * 1.5)
+@IN AOP "Crude Oil" 10.
+@IN AOP "Water" 5.
+@OUT AOP "Heavy Oil" 1.
+@OUT AOP "Light Oil" 4.5
+@OUT AOP "Petroleum Gas" 5.5
+
+recips[HOC] = Recipe("Heavy Oil Cracking", 5.0 , 1.25)
+@IN HOC "Heavy Oil" 4.
+@IN HOC "Water" 3.
+@OUT HOC "Light Oil" 3.
+
+recips[LOC] = Recipe("Light Oil Cracking", 5.0 , 1.25)
+@IN LOC "Light Oil" 3.
+@IN LOC "Water" 3
+@OUT LOC "Petroleum Gas" 2.
+
+recips[SOL] = Recipe("Solid Fuel", 2., 1.25)
+@IN SOL "Petroleum Gas" 2.0
+@OUT SOL "Solid Fuel" 1
+
+recips[ROK] = Recipe("Rocket Fuel Assembly", 30., 1.25)
+@IN ROK "Solid Fuel" 10.
+@OUT ROK "Rocket Fuel" 1
+
+@variable(m, AOPs >= 0, Int)
+@variable(m, HOCs >= 0, Int)
+@variable(m, LOCs >= 0, Int)
+@variable(m, SOLs >= 0, Int)
+@variable(m, ROKs >= 0, Int)
+
+@constraint(m, AOPs * recips[AOP].ins["Crude Oil"] <= 240)
+@constraint(m, HOCs * recips[HOC].ins["Heavy Oil"] <= AOPs * recips[AOP].outs["Heavy Oil"])
+@constraint(m, LOCs * recips[LOC].ins["Light Oil"] <= AOPs * recips[AOP].outs["Light Oil"] + HOCs * recips[HOC].outs["Light Oil"])
+@constraint(m, SOLs * recips[SOL].ins["Petroleum Gas"] <= AOPs * recips[AOP].outs["Petroleum Gas"] + LOCs * recips[LOC].outs["Petroleum Gas"])
+@constraint(m, ROKs * recips[ROK].ins["Solid Fuel"] <= SOLs * recips[SOL].outs["Solid Fuel"])
+
+@objective(m, Max, ROKs)
 
 solve(m)
 
-@printf "oil %f\n" getvalue(oil)
-@printf "h2l %f\n" getvalue(h2l)
-@printf "l2g %f\n" getvalue(l2g)
-@printf "consumes %f oil & produces %f gas per s\n" 10getvalue(oil)/5 (5.5getvalue(oil)+2getvalue(l2g))/5
+production(recips[AOP], getvalue(AOPs))
+production(recips[HOC], getvalue(HOCs))
+production(recips[LOC], getvalue(LOCs))
+production(recips[SOL], getvalue(SOLs))
+production(recips[ROK], getvalue(ROKs))
 
--- steel.jl
+++ steel.jl
@@ -12,8 +12,12 @@ end
 
 
 @enum Item IronPlate IronOre Steel Items
-n = Int64(Items)-1
+@enum Assembler ElecSmelt Assemblers
 
+iNames = Dict{Item, AbstractString}(IronPlate=>"Iron Plate", IronOre=>"Iron Ore", Steel=>"Steel")
+
+n = Int64(Items)-1
+f = Int64(Assemblers)-1
 
 type Recipe
 	outs::Dict{Item, Float64}
@@ -25,8 +29,24 @@ type Recipe
 	end
 end
 
-Steel_Time = 17.5/2
-Iron_Time = 3.5/2
+function production(n::AbstractString, r::Recipe, nfacs)
+	@printf "%s\n" n
+	@printf "\tProduces\n"
+	for (k,v) in r.outs
+		@printf "\t\t%f %s per min\n" 60v * nfacs iNames[k]
+	end
+	@printf "\tConsumes\n"
+	for (k,v) in r.ins
+		@printf "\t\t%f %s per min\n" 60v * nfacs iNames[k]
+	end
+end
+
+facSpeed = Dict{Assembler, Float64}(ElecSmelt=>2.0)
+
+
+
+Steel_Time = 17.5/facSpeed[ElecSmelt]
+Iron_Time = 3.5/facSpeed[ElecSmelt]
 Ore_Time = 1 / 0.525
 
 recips = Dict{Item, Recipe}()
@@ -38,40 +58,15 @@ recips[IronOre] = Recipe(Ore_Time, Dict(IronOre=>1.), Dict(IronOre=>1.))
 @variable(m, facIron >= 1, Int)
 @variable(m, facOre >= 1, Int)
 
-# produces
-Steel_Steel = 1 / Steel_Time
-# consumes
-Steel_IronPlate = 5 / Steel_Time
 
-# produces
-IronPlate_IronPlate = 1 / Iron_Time
-#consumes
-IronPlate_IronOre = 1 / Iron_Time
+@constraint(m, facSteel * recips[Steel].outs[Steel] >= 0)
+@constraint(m, facIron * recips[IronPlate].outs[IronPlate] >= facSteel * recips[Steel].ins[IronPlate])
+@constraint(m, facOre * recips[IronOre].outs[IronOre] >= facIron * recips[IronPlate].ins[IronOre])
 
-#produces
-IronOre_IronOre = 1 / Ore_Time
-
-@constraint(m, facSteel * Steel_Steel >= 0)
-@constraint(m, facIron * IronPlate_IronPlate >= facSteel * Steel_IronPlate)
-@constraint(m, facOre * IronOre_IronOre >= facIron * IronPlate_IronOre)
-
-
-
-#@objective(m, Min, (facIron * IronPlate_IronPlate - facSteel * Steel_IronPlate) + (facOre * IronOre_IronOre - facIron * IronPlate_IronOre))
-
-@objective(m, Min, (facIron * IronPlate_IronPlate - facSteel * Steel_IronPlate))
+@objective(m, Min, (facIron * recips[IronPlate].outs[IronPlate] - facSteel * recips[Steel].ins[IronPlate]))
 
 solve(m)
 
-v1 = getvalue(facSteel)
-r = recips[Steel]
-@printf "Steel factories:%f, making %f steel, consuming %f iron plate\n" v1 v1*r.outs[Steel] v1*r.ins[IronPlate]
-
-v2 = getvalue(facIron)
-r = recips[IronPlate]
-@printf "Iron factories:%f, making %f iron plate, consuming %f iron ore\n" v2 v2*r.outs[IronPlate] v2*r.ins[IronOre]
-
-v3 = getvalue(facOre)
-r = recips[IronOre]
-@printf "Iron mines:%f, making %f iron ore\n" v3 v3*r.outs[IronOre]
-
+production("Steel", recips[Steel], getvalue(facSteel))
+production("Iron Plate", recips[IronPlate], getvalue(facIron))
+production("Iron Ore", recips[IronOre], getvalue(facOre))
