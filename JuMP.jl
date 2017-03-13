46824b053ce91c4a50ce1b7a641d4a9016bfc133
lawless-m <maht0x0r@xsmail.com>
2017-03-13 14:56:16 -0400

    refactored

--- JuMP.jl
+++ JuMP.jl
@@ -15,25 +15,26 @@ end
 recips = Dict{AbstractString, Recipe}()
 
 function production(r::Recipe, nfacs)
+	if nfacs == 0 return end
 	@printf "%s: %d\n" r.name nfacs
 	@printf "\tProduces\n"
 	for (k,v) in r.outs
-		@printf "\t\t%0.2f \"%s\" per min - %f per second\n" 60v * nfacs k v * nfacs
+		@printf "\t\t%0.2f \"%s\" per min - %0.2f per second\n" 60v * nfacs k v * nfacs
 	end
 	@printf "\tConsumes\n"
 	for (k,v) in r.ins
-		@printf "\t\t%0.2f \"%s\" per min - %f per second\n" 60v * nfacs k v * nfacs
+		@printf "\t\t%0.2f \"%s\" per min - %0.2f per second\n" 60v * nfacs k v * nfacs
 	end
 end
 
 
-macro IN(iname, iqty)
+macro RIN(iname, iqty)
 	return quote
 		recipe.ins[$iname] = $iqty / recipe.time
 	end
 end
 
-macro OUT(iname, iqty)
+macro ROUT(iname, iqty)
 	return quote
 		recipe.outs[$iname] = $iqty / recipe.time
 	end
--- modules.jl
+++ modules.jl
@@ -4,59 +4,69 @@ include("JuMP.jl")
 
 const ASS3SPD = 1.25
 const CHEMSPD = 1.25
-const SMELTSPD = 2 
+const SMELTSPD = 2
+const DRILLSPD = 1 / 0.525
 
 recipe = Recipe("Production 1", 15, ASS3SPD)
-@IN "Red Circuit" 5
-@IN "Green Circuit" 5
-@OUT "Production 1" 1
+@RIN "Red Circuit" 5
+@RIN "Green Circuit" 5
+@ROUT "Production 1" 1
 
 recipe = Recipe("Production 2", 30, ASS3SPD)
-@IN "Red Circuit" 5
-@IN "Blue Circuit" 5
-@IN "Production 1" 4
-@OUT "Production 2" 1
+@RIN "Red Circuit" 5
+@RIN "Blue Circuit" 5
+@RIN "Production 1" 4
+@ROUT "Production 2" 1
 
 recipe = Recipe("Production 3", 60, ASS3SPD)
-@IN "Red Circuit" 5
-@IN "Blue Circuit" 5
-@IN "Production 2" 4
-@IN "Alien Artifact" 1
-@OUT "Production 3" 1
+@RIN "Red Circuit" 5
+@RIN "Blue Circuit" 5
+@RIN "Production 2" 4
+@RIN "Alien Artifact" 1
+@ROUT "Production 3" 1
 
 recipe = Recipe("Blue Circuit", 15, ASS3SPD)
-@IN "Green Circuit" 20
-@IN "Red Circuit" 2
-@IN "Sulphuric Acid" 0.5
-@OUT "Blue Circuit" 1
+@RIN "Green Circuit" 20
+@RIN "Red Circuit" 2
+@RIN "Sulphuric Acid" 0.5
+@ROUT "Blue Circuit" 1
 
 recipe = Recipe("Red Circuit", 8, ASS3SPD)
-@IN "Green Circuit" 2
-@IN "Plastic" 2
-@IN "Copper Cable" 4
-@OUT "Red Circuit" 1
+@RIN "Green Circuit" 2
+@RIN "Plastic" 2
+@RIN "Copper Cable" 4
+@ROUT "Red Circuit" 1
 
 recipe = Recipe("Green Circuit", 0.5, ASS3SPD)
-@IN "Iron Plate" 1
-@IN "Copper Cable" 3
-@OUT "Green Circuit" 1
+@RIN "Iron Plate" 1
+@RIN "Copper Cable" 3
+@ROUT "Green Circuit" 1
 
 recipe = Recipe("Copper Cable", 0.5, ASS3SPD)
-@IN "Copper Plate" 1
-@OUT "Copper Cable" 2
+@RIN "Copper Plate" 1
+@ROUT "Copper Cable" 2
 
 recipe = Recipe("Plastic", 1, CHEMSPD)
-@IN "Coal" 1
-@IN "Petroleum Gas" 3
-@OUT "Plastic" 2
+@RIN "Coal" 1
+@RIN "Petroleum Gas" 3
+@ROUT "Plastic" 2
 
 recipe = Recipe("Iron Plate", 3.5, SMELTSPD)
-@IN "Iron Ore" 1
-@OUT "Iron Plate" 1
+@RIN "Iron Ore" 1
+@ROUT "Iron Plate" 1
 
 recipe = Recipe("Copper Plate", 3.5, SMELTSPD)
-@IN "Copper Ore" 1
-@OUT "Copper Plate" 1
+@RIN "Copper Ore" 1
+@ROUT "Copper Plate" 1
+
+recipe = Recipe("Iron Ore", 1, DRILLSPD)
+@RIN "Rock" 1
+@ROUT "Iron Ore" 1
+ 
+recipe = Recipe("Copper Ore", 1, DRILLSPD)
+@RIN "Rock" 1
+@ROUT "Copper Ore" 1
+
 
 m = Model()
 
@@ -70,35 +80,44 @@ m = Model()
 @variable(m, PLASTICs >= 0, Int)
 @variable(m, IRONPs >= 0, Int)
 @variable(m, COPPERPs >= 0, Int)
+@variable(m, IRONOs >= 0, Int)
+@variable(m, COPPEROs >= 0, Int)
 
+Machine = Dict(
+	PROD3s=>"Production 3",
+	PROD2s=>"Production 2",
+	PROD1s=>"Production 1",
+	BLUECs=>"Blue Circuit",
+	REDCs=>"Red Circuit",
+	GREENCs=>"Green Circuit",
+	COPPERCs=>"Copper Cable",
+	PLASTICs=>"Plastic",
+	IRONPs=>"Iron Plate",
+	COPPERPs=>"Copper Plate",
+	IRONOs=>"Iron Ore",
+	COPPEROs=>"Copper Ore"
+	)
 
-macro OUTS(fac, item)
+macro OUT(fac)
 	return quote
-		$fac * recips[$item].outs[$item]
+		$fac * recips[Machine[$fac]].outs[Machine[$fac]]
 	end
 end
-macro INS(fac, recip, item)
+
+macro INS(item)
 	return quote
-		$fac * recips[$recip].ins[$item]
+		sum([OtherM * get(recips[Machine[OtherM]].ins, Item, 0) for OtherM in collect(keys(Machine))])
 	end
 end
 
-@constraint(m, @OUTS(PROD3s, "Production 3") >= 10/60)
-@constraint(m, @OUTS(PROD2s, "Production 2") >= @INS(PROD3s, "Production 3", "Production 2"))
-@constraint(m, @OUTS(PROD1s, "Production 1") >= @INS(PROD2s, "Production 2", "Production 1"))
-@constraint(m, @OUTS(BLUECs, "Blue Circuit") >= @INS(PROD2s, "Production 2", "Blue Circuit") + @INS(PROD3s, "Production 3", "Blue Circuit"))
-@constraint(m, @OUTS(REDCs, "Red Circuit") >= @INS(PROD2s, "Production 2", "Red Circuit") + @INS(PROD3s, "Production 3", "Red Circuit") + @INS(BLUECs, "Blue Circuit", "Red Circuit"))
-@constraint(m, @OUTS(GREENCs, "Green Circuit") >= @INS(REDCs, "Red Circuit", "Green Circuit") + @INS(PROD1s, "Production 1", "Green Circuit") + @INS(BLUECs, "Blue Circuit", "Green Circuit"))
-@constraint(m, @OUTS(COPPERCs, "Copper Cable") >= @INS(GREENCs, "Green Circuit", "Copper Cable") + @INS(REDCs, "Red Circuit", "Copper Cable"))
-@constraint(m, @OUTS(PLASTICs, "Plastic") >= @INS(REDCs, "Red Circuit", "Plastic"))
-@constraint(m, @OUTS(IRONPs, "Iron Plate") >= @INS(GREENCs, "Green Circuit", "Iron Plate"))
-@constraint(m, @OUTS(COPPERPs, "Copper Plate") >= @INS(COPPERCs, "Copper Cable", "Copper Plate"))
-
-
-@objective(m, Min, PROD1s + PROD2s + PROD3s + BLUECs + REDCs + GREENCs + COPPERCs + PLASTICs + IRONPs + COPPERPs)
+@constraint(m, @OUT(PROD3s) >= 1)
+for (M,Item) in Machine
+	@constraint(m, @OUT(M) >= @INS N)
+end
+@objective(m, Min, sum(collect(keys(Machine))))
 
 solve(m)
 
-for (r, n) in [("Iron Plate", IRONPs) ("Copper Plate", COPPERPs) ("Copper Cable", COPPERCs) ("Green Circuit", GREENCs) ("Blue Circuit", BLUECs) ("Plastic", PLASTICs) ("Red Circuit", REDCs) ("Production 1", PROD1s) ("Production 2", PROD2s) ("Production 3", PROD3s)]
-	production(recips[r], getvalue(n))
+for m in [IRONOs COPPEROs IRONPs COPPERPs COPPERCs GREENCs BLUECs PLASTICs REDCs PROD1s PROD2s PROD3s]
+	production(recips[Machine[m]], getvalue(m))
 end
