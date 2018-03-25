2ff9a83addedbda01ef7f8ec055d8bf56c5cab80
lawless-m <matt-github@steponnopets.net>
2018-03-25 21:09:42 +0100

    fixed for 0.15 & 0.16

--- LuaRecipe_to_JL.lua
+++ LuaRecipe_to_JL.lua
@@ -1,110 +1,119 @@
+#!/usr/bin/lua5.3
 
 recipfns = {
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\ammo.lua",
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\capsule.lua", 
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\equipment.lua", 
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\fluid-recipe.lua",
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\furnace-recipe.lua", 
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\inserter.lua",
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\module.lua",
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\recipe.lua", 
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\turret.lua",
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\demo-recipe.lua",
-  "C:\\Users\\mwheath\\Documents\\Factorio\\prototypes\\recipe\\demo-furnace-recipe.lua"
+  "/home/matt/factorio/data/base/prototypes/recipe/ammo.lua",
+  "/home/matt/factorio/data/base/prototypes/recipe/capsule.lua", 
+  "/home/matt/factorio/data/base/prototypes/recipe/equipment.lua", 
+  "/home/matt/factorio/data/base/prototypes/recipe/fluid-recipe.lua",
+  "/home/matt/factorio/data/base/prototypes/recipe/furnace-recipe.lua", 
+  "/home/matt/factorio/data/base/prototypes/recipe/inserter.lua",
+  "/home/matt/factorio/data/base/prototypes/recipe/module.lua",
+  "/home/matt/factorio/data/base/prototypes/recipe/recipe.lua", 
+  "/home/matt/factorio/data/base/prototypes/recipe/turret.lua",
+  "/home/matt/factorio/data/base/prototypes/recipe/demo-recipe.lua",
+  "/home/matt/factorio/data/base/prototypes/recipe/demo-furnace-recipe.lua"
 }
 
-JuMPfn = "recipe_protos.jl"
 
-rfid = io.open(JuMPfn, "w+")
-
-
-function getrecips(fn)
-	fid = io.open(fn)
-	
-	loader = function()
-		txt = fid:read()
-		print(txt)
-		if txt == "data:extend(" then
-			txt = "return "
-		elseif txt == ")" then
-			txt = nil
-		elseif txt == "})" then
-			txt = "}"
-		elseif txt == "" then
-			txt = loader()
+function wrt(fid, n, f, t, bits, res, rescnt)
+	fid:write("recipe = Recipe{" .. f .. "}(\"" .. n .. "\", ".. tostring(t) .. ")\n")
+	for i = 1, #bits do
+		printIngs(fid, "@RIN", bits[i])
+	end
+	if type(res) == "table" then
+		for i = 1, #res do
+			printIngs(fid, "@ROUT", res[i])
 		end
-		print(txt)
-		return txt
+	else
+		if rescnt ~= nil then
+			a = tostring(rescnt)
+		else
+			a = "1"
+		end
+		fid:write("@ROUT \"" .. res .. "\" " .. a .. "\n")
 	end
 
-	f = load(loader)
-	fid:close()
-	return f()
 end
 
-function printIngs(dir, igs)
+data = {}
+data["cheap"] = io.open("recipe_protos_cheap.jl", "w+")
+data["expensive"] = io.open("recipe_protos_expensive.jl", "w+")
+data["extend"] = function(data, recips)
+	for r = 1,#recips do
+		if recips[r]["type"] == "recipe" then
+
+			if recips[r]["name"] == "basic-oil-processing" then
+				f = "OilBasic"
+			elseif recips[r]["name"] == "advanced-oil-processing" then
+				f = "OilAdvanced"
+			elseif recips[r]["category"] == "chemistry" then
+				f = "ChemPlant"
+			elseif recips[r]["category"] == "smelting" then
+				f = "Smelter"
+			else
+				f = "Assembler"
+			end
+
+			print(recips[r]["name"] )
+
+			if recips[r]["ingredients"] == nil then
+				t = recips[r]["normal"]["energy_required"]
+				if t == nil then
+					t = 0.5
+				end
+				wrt(data["cheap"], recips[r]["name"], f, t, recips[r]["normal"]["ingredients"], recips[r]["normal"]["results"] or recips[r]["normal"]["result"], recips[r]["result_count"])
+
+				t = recips[r]["expensive"]["energy_required"]
+				if t == nil then
+					t = 0.5
+				end
+				wrt(data["expensive"], recips[r]["name"], f, t, recips[r]["expensive"]["ingredients"], recips[r]["expensive"]["results"] or recips[r]["expensive"]["result"], recips[r]["result_count"])
+
+
+			else
+				t = recips[r]["energy_required"]
+				if t == nil then
+					t = 0.5
+				end
+				wrt(data["cheap"], recips[r]["name"], f, t, recips[r]["ingredients"], recips[r]["results"] or recips[r]["result"], recips[r]["result_count"])
+				wrt(data["expensive"], recips[r]["name"], f, t, recips[r]["ingredients"], recips[r]["results"] or recips[r]["result"], recips[r]["result_count"])
+
+			end
+
+			data["cheap"]:write("\n")
+			data["expensive"]:write("\n")
+		end
+	end
+end
+
+
+
+function printIngs(fid, dir, igs)
 	if igs["type"] == "fluid" or igs["type"] == "item" then
 		k = igs["name"]
 		a = igs["amount"]
-	else
+	elseif igs["probability"] == nil then
 		k = igs[1]
-		a = igs[2]			
+		a = igs[2]
+		if k == nil then
+			k = igs["name"]
+			a = igs["amount"]
+		end
+	else
+		k = igs["name"]
+		a = igs["probability"] * igs["amount"]
 	end
-	rfid:write(dir .. " \"" .. k .. "\" " .. tostring(a) .. "\n")
+	fid:write(dir .. " \"" .. k .. "\" " .. tostring(a) .. "\n")
 end
 
 function writeRecips()
 
 	for fn = 1,#recipfns do
+		print(recipfns[fn])
+		f = loadfile(recipfns[fn])
+		f()
+		print("procced")
 		
-		recips = getrecips(recipfns[fn])
-		if recips == nil then
-			rfid:write("nil from " .. recipfns[fn] .. "\n")
-		else
-			for r = 1,#recips do
-				if recips[r]["type"] == "recipe" then
-					t = recips[r]["energy_required"]
-					if t == nil then
-						t = 0.5
-					end
-					if recips[r]["name"] == "basic-oil-processing" then
-						f = "OilBasic"
-					elseif recips[r]["name"] == "advanced-oil-processing" then
-						f = "OilAdvanced"
-					elseif recips[r]["category"] == "chemistry" then
-						f = "ChemPlant"
-					elseif recips[r]["category"] == "smelting" then
-						f = "Smelter"
-					else
-						f = "Assembler"
-					end
-					
-					rfid:write("recipe = Recipe{" .. f .. "}(\"" .. recips[r]["name"] .. "\", ".. tostring(t) .. ")\n")
-					
-					
-					for i = 1, #recips[r]["ingredients"] do
-						printIngs("@RIN", recips[r]["ingredients"][i])
-					end
-					
-					res = recips[r]["results"] or recips[r]["result"]
-					
-					if type(res) == "table" then
-						for i = 1, #res do
-							printIngs("@ROUT", res[i])
-						end
-					else
-						if recips[r]["result_count"] ~= nil then
-							a = tostring(recips[r]["result_count"])
-						else
-							a = "1"
-						end
-						rfid:write("@ROUT \"" .. res .. "\" " .. a .. "\n")
-					end
-					
-					rfid:write("\n")
-				end
-			end
-		end
 	end
 end
 
